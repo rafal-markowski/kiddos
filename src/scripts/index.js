ba15ac37a0f0373ca4cf789efc7f136879fd2608
rafal <rafal_markowski@onet.pl>
2019-03-30 10:39:13 +0100

    Dodanie sekcji z oferowanymi usługami

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,3 +6,7 @@ import './header';
 
 import '../images/carousel/slide_1.jpg';
 import '../images/carousel/slide_2.jpg';
+import '../images/icons/teacher.svg';
+import '../images/icons/reading.svg';
+import '../images/icons/book.svg';
+import '../images/icons/certificate.svg';
