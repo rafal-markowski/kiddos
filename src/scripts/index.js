2e7f924184a9e6ab48805b7ebeb0f83c8caca005
rafal <rafal_markowski@onet.pl>
2019-04-01 15:44:14 +0200

    Wprowadzenie biblioteki aos

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,8 @@
 import "bootstrap";
+import AOS from 'aos';
 
 import '../styles/main.scss';
+import 'aos/dist/aos.css'
 
 import './header';
 
@@ -12,3 +14,10 @@ import '../images/icons/book.svg';
 import '../images/icons/certificate.svg';
 import '../images/blog/img_1.jpg';
 import '../images/blog/img_2.jpg';
+
+AOS.init({
+    disable: 'mobile',
+    offset: 0,
+    once: true,
+    useClassNames: false
+});
