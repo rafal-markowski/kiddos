dde861c5c122a9efb7c7dca1d065960278fc281b
rafal <rafal_markowski@onet.pl>
2019-04-07 22:58:07 +0200

    Stworzenie galerii z listą nauczycieli

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -15,6 +15,15 @@ import '../images/icons/book.svg';
 import '../images/icons/certificate.svg';
 import '../images/blog/img_1.jpg';
 import '../images/blog/img_2.jpg';
+import '../images/teacher/img_1.jpg';
+import '../images/teacher/img_2.jpg';
+import '../images/teacher/img_3.jpg';
+import '../images/teacher/img_4.jpg';
+import '../images/teacher/img_5.jpg';
+import '../images/teacher/img_6.jpg';
+import '../images/teacher/img_7.jpg';
+import '../images/teacher/img_8.jpg';
+
 
 AOS.init({
     disable: 'mobile',
